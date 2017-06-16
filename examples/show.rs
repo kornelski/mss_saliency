25234015c6827ac4580d835e52f49441683374a1
Kornel <kornel@geekhood.net>
2017-06-17 00:14:07 +0100

    Added example

---
+++ examples/show.rs
@@ -0,0 +1,24 @@
+extern crate mss_saliency;
+extern crate lodepng;
+extern crate imgref;
+
+use std::env;
+use std::cmp;
+use std::path::PathBuf;
+
+fn main() {
+    let path = PathBuf::from(env::args_os().nth(1).expect("Please provide a PNG file as an argument"));
+    let img = lodepng::decode32_file(&path).unwrap();
+    let fast_gray: Vec<_> = img.buffer.as_ref().iter().map(|px| {
+        (px.a as u32 * (px.r as u32 + px.g as u32 * 2 + px.b as u32) / (4*256)) as u8
+    }).collect();
+
+    let gray_img = imgref::ImgRef::new(&fast_gray, img.width, img.height);
+
+    let sal = mss_saliency::maximum_symmetric_surround_saliency(gray_img);
+
+    let rgb: Vec<_> = sal.buf.iter().map(|&v| (cmp::min(255,v>>6) as u8,(v>>8) as u8,cmp::min(255,v>>2) as u8)).collect();
+    let new_path = format!("{}-mss-saliency.png", path.display());
+    lodepng::encode24_file(&new_path, &rgb, sal.width as usize, sal.height as usize).unwrap();
+    println!("Wrote {}", new_path);
+}
